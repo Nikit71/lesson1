8362a11913d39817406c3cfd3e2452b210d7bb98
Nikit71 <nikitural@gmail.com>
2024-10-11 23:55:13 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,32 @@
+# Динамическая типизация
+print()
+print('Практическое задание по уроку "Динамическая типизация"')
+name='Евгений'
+print('Name: ',name)
+age=53
+print('Age: ', age)
+age=age+1
+print('New Age: ', age)
+is_student= True
+print ('Is student: ', is_student)
+
+# Переменные
+print()
+print()
+print('Практическое задание по уроку "Переменные"')
+number_of_completed_tasks=12
+number_of_hours_spent=1.5
+name_kurs='Python'
+time_to_complete_one_task=number_of_hours_spent/number_of_completed_tasks
+print('Курс:', name_kurs, ', всего задач:', number_of_completed_tasks, ', затрачено часов:', number_of_hours_spent, ', среднее время выполнения ', time_to_complete_one_task)
+
+# Строки и индексация строк
+print()
+print()
+print('Практическое задание по уроку "Строки и индексация строк"')
+example='гипопотам'
+print(example[0])
+print(example[-1])
+print(example[len(example)//2:])
+print(example[::-1])
+print(example[1::2])
